dff19cacea752e3a52008e20a19f0e0241f51fe8
Mit Dave <mitdave95@gmail.com>
2024-10-05 11:39:26 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase Config
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyAXCU8uu83AiwsmHpg2RxsgBWHKf3jIoKM",
+  authDomain: "omniplex-a28a0.firebaseapp.com",
+  projectId: "omniplex-a28a0",
+  storageBucket: "omniplex-a28a0.appspot.com",
+  messagingSenderId: "706815869863",
+  appId: "1:706815869863:web:3557a96241c09d8d066c82",
+  measurementId: "G-H10HLYG0G2"
 };
 
 const app = initializeApp(firebaseConfig);
